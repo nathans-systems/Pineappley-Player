6e9d5dc05679cb8352229fef62a47f17d9df90eb
Nathan S <64860737+nathans-systems@users.noreply.github.com>
2026-09-04 20:14:48 +0100

    Update main.cpp
    firmware update

--- Code/mp3_player/src/main.cpp
+++ Code/mp3_player/src/main.cpp
@@ -6,7 +6,6 @@
 #include "Audio.h"
 #include <vector>
 
-
 #define OLED_SDA      1
 #define OLED_SCL      2
 
@@ -25,11 +24,10 @@
 #define ENCODER_OUTB  13
 #define ENCODER_SW    10
 
-
 enum UIState { SCREEN_MENU, SCREEN_PLAYER, SCREEN_HELP };
 UIState currentScreen = SCREEN_MENU;
 
-enum PlayerButton { BTN_PREV, BTN_PLAY_PAUSE, BTN_NEXT, BTN_SHUFFLE };
+enum PlayerButton { BTN_PREV, BTN_PLAY_PAUSE, BTN_NEXT, BTN_SHUFFLE, BTN_SEEK };
 PlayerButton selectedPlayerBtn = BTN_PLAY_PAUSE;
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
@@ -42,46 +40,50 @@ int menuScrollOffset = 0;
 bool isPlaying = false;
 bool isShuffle = false;
 
-
 const float MAX_SAFE_VOLUME = 4.0;
 int currentVolume = 50;
 bool showVolumeOverlay = false;
 
+bool isSeeking = false;
+uint32_t seekTargetSec = 0;
 
 int batteryPercentage = 100;
+float filteredVoltageMv = 0.0;
 unsigned long lastBatteryCheck = 0;
 
-//volume percentage
 void updateAudioVolume() {
   float targetVol = (currentVolume / 100.0f) * MAX_SAFE_VOLUME;
   audio.setVolume(targetVol);
 }
 
-//convert volts to percent using discharge curve
 int calculateLiPoPercentage(uint32_t mVolts) {
-  if (mVolts >= 4200) return 100;
-  if (mVolts >= 4050) return map(mVolts, 4050, 4200, 85, 100);
-  if (mVolts >= 3800) return map(mVolts, 3800, 4050, 55, 85);
-  if (mVolts >= 3700) return map(mVolts, 3700, 3800, 20, 55);
-  if (mVolts >= 3500) return map(mVolts, 3500, 3700, 5, 20);
-  if (mVolts >= 3300) return map(mVolts, 3300, 3500, 0, 5);
-  return 0;
+  const uint32_t MIN_VOLTAGE = 3200;
+  const uint32_t MAX_VOLTAGE = 4200;
+
+  if (mVolts >= MAX_VOLTAGE) return 100;
+  if (mVolts <= MIN_VOLTAGE) return 0;
+
+  return (int)(((float)(mVolts - MIN_VOLTAGE) / (MAX_VOLTAGE - MIN_VOLTAGE)) * 100.0f);
 }
 
-//read battery
 void readBatteryLevel() {
   uint32_t rawSum = 0;
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 32; i++) {
     rawSum += analogReadMilliVolts(BATTERY_PIN);
   }
-  uint32_t measuredPinmV = rawSum / 16;
+  float currentPinMv = (float)rawSum / 32.0f;
+  
+  float currentBatMv = currentPinMv * 2.0f;
 
-  uint32_t batteryVoltage = measuredPinmV * 2; 
+  if (filteredVoltageMv == 0.0) {
+    filteredVoltageMv = currentBatMv;
+  } else {
+    filteredVoltageMv = (filteredVoltageMv * 0.85f) + (currentBatMv * 0.15f);
+  }
 
-  batteryPercentage = calculateLiPoPercentage(batteryVoltage);
+  batteryPercentage = calculateLiPoPercentage((uint32_t)filteredVoltageMv);
 }
 
-//render battery
 void drawBatteryIndicator() {
   u8g2.setFont(u8g2_font_micro_tr);
   
@@ -102,7 +104,6 @@ void drawBatteryIndicator() {
 volatile int encoderStepDelta = 0; 
 portMUX_TYPE encoderMux = portMUX_INITIALIZER_UNLOCKED;
 
-//rotary encoder interrupt
 void IRAM_ATTR encoderISR() {
   static uint8_t oldState = 0;
   static const int8_t knobStates[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
@@ -121,6 +122,10 @@ void IRAM_ATTR encoderISR() {
 }
 
 bool lastRawBtnState = HIGH;
+bool stableBtnState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long DEBOUNCE_DELAY = 40;
+
 unsigned long btnPressStartTime = 0;
 int clickCount = 0;
 unsigned long lastClickReleaseTime = 0;
@@ -132,7 +137,7 @@ int marqueePosPlayer = 0;
 unsigned long lastMarqueeUpdate = 0;
 
 uint8_t barHeights[14] = {0};
-//pineapple :)
+
 const unsigned char epd_bitmap_pineapple_64x64 [] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x00, 
   0x00, 0x00, 0x00, 0xc0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7c, 0xe0, 0x07, 0x3e, 0x00, 0x00, 
@@ -144,7 +149,8 @@ const unsigned char epd_bitmap_pineapple_64x64 [] PROGMEM = {
   0x00, 0x80, 0xff, 0xc7, 0xe3, 0xff, 0x01, 0x00, 0x00, 0xc0, 0xe3, 0xcf, 0xf3, 0xc7, 0x03, 0x00, 
   0x00, 0xe0, 0x03, 0xfe, 0x7f, 0xc0, 0x07, 0x00, 0x00, 0xc0, 0x07, 0xf8, 0x1f, 0xe0, 0x03, 0x00, 
   0x00, 0x80, 0x0f, 0xf0, 0x0f, 0xf0, 0x01, 0x00, 0x00, 0x00, 0x1e, 0xe0, 0x07, 0x78, 0x00, 0x00, 
-  0x00, 0x00, 0x3c, 0xc0, 0x03, 0x3c, 0x00, 0x00, 0x00, 0x00, 0x78, 0xc0, 0x03, 0x1e, 0x00, 0x00, 0x00, 0x00, 0xf0, 0xfc, 0x3f, 0x0f, 0x00, 0x00, 0x00, 0x00, 0xe0, 0xff, 0xff, 0x07, 0x00, 0x00, 
+  0x00, 0x00, 0x3c, 0xc0, 0x03, 0x3c, 0x00, 0x00, 0x00, 0x00, 0x78, 0xc0, 0x03, 0x1e, 0x00, 0x00, 
+  0x00, 0x00, 0xf0, 0xfc, 0x3f, 0x0f, 0x00, 0x00, 0x00, 0x00, 0xe0, 0xff, 0xff, 0x07, 0x00, 0x00, 
   0x00, 0x00, 0xc0, 0x1f, 0xf8, 0x03, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x03, 0xc0, 0x03, 0x00, 0x00, 
   0x00, 0x00, 0xe0, 0x03, 0xc0, 0x07, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x07, 0xe0, 0x0f, 0x00, 0x00, 
   0x00, 0x00, 0x38, 0x0f, 0xf0, 0x1c, 0x00, 0x00, 0x00, 0x00, 0x3c, 0x1e, 0x78, 0x3c, 0x00, 0x00, 
@@ -154,8 +160,7 @@ const unsigned char epd_bitmap_pineapple_64x64 [] PROGMEM = {
   0x00, 0x80, 0x03, 0xf0, 0x0f, 0xc0, 0x01, 0x00, 0x00, 0x80, 0x07, 0x78, 0x1e, 0xe0, 0x01, 0x00, 
   0x00, 0x80, 0x0f, 0x3c, 0x3c, 0xf0, 0x01, 0x00, 0x00, 0xc0, 0x1f, 0x1e, 0x78, 0xf8, 0x03, 0x00, 
   0x00, 0xc0, 0x39, 0x0f, 0xf0, 0x9c, 0x03, 0x00, 0x00, 0xc0, 0xf1, 0x07, 0xe0, 0x8f, 0x03, 0x00, 
-  0x00, 0xc0, 0xe1, 0x03, 0xc0, 0x87, 0x03, 0x00, 0x00, 0xc0, 0xe1, 0x03, 0xc0, 0x87, 0x03, 0x00, 
-  0x00, 0xc0, 0xf1, 0x03, 0xc0, 0x8f, 0x03, 0x00, 0x00, 0xc0, 0xf9, 0x07, 0xe0, 0x9f, 0x03, 0x00, 
+  0x00, 0xc0, 0xe1, 0x03, 0xc0, 0x87, 0x03, 0x00, 0x00, 0xc0, 0xe1, 0x03, 0xc0, 0x87, 0x03, 0x00, 0x00, 0xc0, 0xf1, 0x03, 0xc0, 0x8f, 0x03, 0x00, 0x00, 0xc0, 0xf9, 0x07, 0xe0, 0x9f, 0x03, 0x00, 
   0x00, 0xc0, 0x3d, 0x0e, 0xf0, 0xbc, 0x03, 0x00, 0x00, 0xc0, 0x1f, 0x1c, 0x78, 0xf8, 0x03, 0x00, 
   0x00, 0xc0, 0x0f, 0x38, 0x3c, 0xf0, 0x03, 0x00, 0x00, 0x80, 0x07, 0x70, 0x1e, 0xe0, 0x01, 0x00, 
   0x00, 0x80, 0x03, 0xe0, 0x0f, 0xc0, 0x01, 0x00, 0x00, 0x80, 0x03, 0xc0, 0x03, 0xc0, 0x01, 0x00, 
@@ -167,7 +172,6 @@ const unsigned char epd_bitmap_pineapple_64x64 [] PROGMEM = {
   0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x0f, 0x00, 0x00, 0x00
 };
 
-//background core 0 task
 void AudioTask(void *pvParameters) {
   for (;;) {
     audio.loop();
@@ -182,7 +186,6 @@ void showSplashScreen() {
   delay(50); 
 }
 
-//keep only filename, stripping path
 String cleanFileName(String path) {
   int idx = path.lastIndexOf('/');
   if (idx != -1) path = path.substring(idx + 1);
@@ -197,7 +200,6 @@ String formatTime(uint32_t seconds) {
   return String(buf);
 }
 
-//scan sd root and get files
 void scanSDCard() {
   File root = SD.open("/");
   if (!root) return;
@@ -216,7 +218,6 @@ void scanSDCard() {
   }
 }
 
-//initiate audio playback
 void playTrack(int index) {
   if (songList.empty()) return;
   currentTrackIndex = index;
@@ -226,6 +227,7 @@ void playTrack(int index) {
   audio.connecttoFS(SD, path.c_str());
   isPlaying = true;
   marqueePosPlayer = 0;
+  isSeeking = false;
   currentScreen = SCREEN_PLAYER;
 }
 
@@ -245,7 +247,6 @@ void togglePlayPause() {
   }
 }
 
-//pick next song
 int getNextTrackIndex() {
   if (songList.empty()) return 0;
   if (isShuffle && songList.size() > 1) {
@@ -258,7 +259,6 @@ int getNextTrackIndex() {
   return (playingTrackIndex + 1) % songList.size();
 }
 
-//ui scrolling through files
 void drawMenuUI() {
   u8g2.setFont(u8g2_font_6x10_tf);
 
@@ -294,7 +294,7 @@ void drawMenuUI() {
 
     if (itemIdx == currentTrackIndex) {
       u8g2.drawBox(0, y - 8, 128, 12);
-      u8g2.setDrawColor(0); 
+      u8g2.setDrawColor(0); // Invert text for selected item
 
       if (trackName.length() > 25) {
         String doubled = trackName + "   " + trackName;
@@ -303,7 +303,7 @@ void drawMenuUI() {
       } else {
         u8g2.drawStr(2, y, trackName.c_str());
       }
-      u8g2.setDrawColor(1); 
+      u8g2.setDrawColor(1); // Reset
     } else {
       if (trackName.length() > 25) trackName = trackName.substring(0, 22) + "..";
       u8g2.drawStr(2, y, trackName.c_str());
@@ -311,7 +311,6 @@ void drawMenuUI() {
   }
 }
 
-//render ui elements
 void drawPlayerUI() {
   u8g2.setFont(u8g2_font_6x10_tf);
 
@@ -325,7 +324,7 @@ void drawPlayerUI() {
   }
   u8g2.drawHLine(0, 13, 128);
 
-  uint32_t curSec = audio.getAudioCurrentTime();
+  uint32_t curSec = isSeeking ? seekTargetSec : audio.getAudioCurrentTime();
   uint32_t totSec = audio.getAudioFileDuration();
   
   u8g2.drawStr(0, 25, formatTime(curSec).c_str());
@@ -337,7 +336,6 @@ void drawPlayerUI() {
   }
   u8g2.drawStr(96, 25, formatTime(totSec).c_str());
 
-  //generate equaliser
   uint16_t vu = audio.getVUlevel(); 
   uint8_t leftChannel = vu >> 8;
   uint8_t rightChannel = vu & 0xFF;
@@ -367,68 +365,96 @@ void drawPlayerUI() {
     if (targetHeight >= barHeights[b]) {
       barHeights[b] = targetHeight;
     } else {
-      if (barHeights[b] > 1) barHeights[b]--; 
+      if (barHeights[b] > 1) barHeights[b]--; // Decay bar
     }
 
     u8g2.drawBox(6 + (b * 9), 45 - barHeights[b], 5, barHeights[b]);
   }
 
-
+  // BTN_PREV
   u8g2.setDrawColor(1);
   if (selectedPlayerBtn == BTN_PREV) {
-    u8g2.drawBox(4, 49, 26, 14);
+    u8g2.drawBox(2, 49, 22, 14);
     u8g2.setDrawColor(0);
   }
-  u8g2.drawVLine(10, 52, 8);
+  u8g2.drawVLine(6, 52, 8);
+  u8g2.drawTriangle(12, 52, 12, 59, 8, 55);
   u8g2.drawTriangle(17, 52, 17, 59, 13, 55);
-  u8g2.drawTriangle(23, 52, 23, 59, 18, 55);
 
+  // BTN_PLAY_PAUSE
   u8g2.setDrawColor(1);
   if (selectedPlayerBtn == BTN_PLAY_PAUSE) {
-    u8g2.drawBox(35, 49, 26, 14);
+    u8g2.drawBox(27, 49, 22, 14);
     u8g2.setDrawColor(0);
   }
   if (isPlaying) {
-    u8g2.drawBox(44, 52, 3, 8);
-    u8g2.drawBox(49, 52, 3, 8);
+    u8g2.drawBox(33, 52, 3, 8);
+    u8g2.drawBox(38, 52, 3, 8);
   } else {
-    u8g2.drawTriangle(45, 52, 45, 59, 52, 55);
+    u8g2.drawTriangle(34, 52, 34, 59, 41, 55);
   }
 
+  // BTN_NEXT
   u8g2.setDrawColor(1);
   if (selectedPlayerBtn == BTN_NEXT) {
-    u8g2.drawBox(66, 49, 26, 14);
+    u8g2.drawBox(52, 49, 22, 14);
     u8g2.setDrawColor(0);
   }
-  u8g2.drawTriangle(72, 52, 72, 59, 77, 55);
-  u8g2.drawTriangle(78, 52, 78, 59, 83, 55);
-  u8g2.drawVLine(85, 52, 8);
+  u8g2.drawTriangle(56, 52, 56, 59, 61, 55);
+  u8g2.drawTriangle(62, 52, 62, 59, 67, 55);
+  u8g2.drawVLine(69, 52, 8);
 
+  // BTN_SHUFFLE
   u8g2.setDrawColor(1);
   if (selectedPlayerBtn == BTN_SHUFFLE) {
-    u8g2.drawBox(97, 49, 26, 14);
+    u8g2.drawBox(77, 49, 22, 14);
     u8g2.setDrawColor(0);
   }
-  
-  u8g2.drawLine(103, 53, 108, 58);
-  u8g2.drawLine(103, 58, 108, 53);
-  u8g2.drawPixel(109, 53);        
-  u8g2.drawPixel(109, 54);
-  u8g2.drawPixel(109, 58);        
-  u8g2.drawPixel(109, 57);
-
+  u8g2.drawLine(82, 53, 87, 58);
+  u8g2.drawLine(82, 58, 87, 53);
   if (isShuffle) {
-    u8g2.drawHLine(103, 60, 14);
+    u8g2.drawHLine(82, 60, 12);
   }
 
+  // BTN_SEEK (Search Icon)
+  u8g2.setDrawColor(1);
+  if (selectedPlayerBtn == BTN_SEEK) {
+    u8g2.drawBox(102, 49, 24, 14);
+    u8g2.setDrawColor(0);
+  }
+  u8g2.drawCircle(111, 55, 3);
+  u8g2.drawLine(113, 57, 117, 61);
+
   u8g2.setDrawColor(1); 
+}
+
+void drawSeekOverlay() {
+  u8g2.drawBox(10, 15, 108, 36);
+  u8g2.setDrawColor(0);
+  u8g2.drawFrame(11, 16, 106, 34);
+
+  u8g2.setFont(u8g2_font_micro_tr);
+  u8g2.drawStr(18, 25, "SEEKING TRACK...");
+
+  u8g2.drawFrame(18, 30, 72, 10);
+  uint32_t totSec = audio.getAudioFileDuration();
+  if (totSec > 0) {
+    int fill = map(seekTargetSec, 0, totSec, 0, 68);
+    u8g2.drawBox(20, 32, fill, 6);
+  }
+
+  u8g2.setFont(u8g2_font_micro_tr);
+  u8g2.drawStr(18, 46, formatTime(seekTargetSec).c_str());
+  u8g2.drawStr(65, 46, ("/ " + formatTime(totSec)).c_str());
+
+  u8g2.setDrawColor(1);
 }
 
 void drawHelpUI() {
   u8g2.setFont(u8g2_font_micro_tr);
   u8g2.drawStr(24, 8, "=== DEVICE HELP ===");
   u8g2.drawStr(0, 20, "* Rotate: Scroll Menu / Select");
-  u8g2.drawStr(0, 30, "* 1-Click: Select / Play / Pause");
+  u8g2.drawStr(0, 30, "* 1-Click: Select / Seek Confirm");
   u8g2.drawStr(0, 40, "* 2-Clicks: Return to Main Menu");
   u8g2.drawStr(0, 50, "* 3-Clicks: Open Info Screen");
   u8g2.drawStr(0, 60, "* Hold 1.5s: Show/Hide Volume");
@@ -452,7 +478,6 @@ void drawVolumeOverlay() {
   u8g2.setDrawColor(1); 
 }
 
-//routes ui state and calls buffer
 void renderUI() {
   u8g2.clearBuffer();
 
@@ -464,7 +489,9 @@ void renderUI() {
 
   drawBatteryIndicator();
 
-  if (showVolumeOverlay) {
+  if (isSeeking) {
+    drawSeekOverlay();
+  } else if (showVolumeOverlay) {
     drawVolumeOverlay();
   }
 
@@ -516,7 +543,6 @@ void setup() {
 
   readBatteryLevel();
 
-  //create task and pin to core 0
   xTaskCreatePinnedToCore(
     AudioTask,
     "AudioTask",
@@ -542,13 +568,12 @@ void loop() {
     renderUI();
   }
 
-  //convert raw steps into movement
   int steps = 0;
   if (encoderStepDelta != 0) {
     portENTER_CRITICAL(&encoderMux);
     steps = encoderStepDelta / 4; 
     if (steps != 0) {
-      encoderStepDelta %= 4;     
+      encoderStepDelta %= 4;      
     }
     portEXIT_CRITICAL(&encoderMux);
   }
@@ -558,7 +583,15 @@ void loop() {
     int stepMagnitude = abs(steps);
 
     for (int i = 0; i < stepMagnitude; i++) {
-      if (showVolumeOverlay) {
+      if (isSeeking) {
+        uint32_t total = audio.getAudioFileDuration();
+        if (isCW) {
+          seekTargetSec = min(total, seekTargetSec + 5);
+        } else {
+          seekTargetSec = (seekTargetSec > 5) ? seekTargetSec - 5 : 0;
+        }
+      }
+      else if (showVolumeOverlay) {
         if (isCW) currentVolume = min(100, currentVolume + 2);
         else      currentVolume = max(0, currentVolume - 2);
         
@@ -574,43 +607,64 @@ void loop() {
       } 
       else if (currentScreen == SCREEN_PLAYER) {
         if (isCW) {
-          selectedPlayerBtn = (PlayerButton)((selectedPlayerBtn + 1) % 4);
+          selectedPlayerBtn = (PlayerButton)((selectedPlayerBtn + 1) % 5);
         } else {
-          selectedPlayerBtn = (PlayerButton)((selectedPlayerBtn + 3) % 4);
+          selectedPlayerBtn = (PlayerButton)((selectedPlayerBtn + 4) % 5);
         }
       }
     }
   }
 
-  //press detection
-  bool currentRawBtn = digitalRead(ENCODER_SW);
+  bool rawReading = digitalRead(ENCODER_SW);
 
-  if (currentRawBtn == LOW && lastRawBtnState == HIGH) {
-    btnPressStartTime = millis();
-    longPressTriggered = false;
+  if (rawReading != lastRawBtnState) {
+    lastDebounceTime = millis();
   }
 
-  //1.5s press
-  if (currentRawBtn == LOW && !longPressTriggered) {
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+    if (rawReading != stableBtnState) {
+      stableBtnState = rawReading;
+
+      if (stableBtnState == LOW) {
+        btnPressStartTime = millis();
+        longPressTriggered = false;
+      } else {
+        if (!longPressTriggered) {
+          clickCount++;
+          lastClickReleaseTime = millis();
+        }
+      }
+    }
+  }
+  lastRawBtnState = rawReading;
+
+  if (stableBtnState == LOW && !longPressTriggered) {
     if (millis() - btnPressStartTime >= 1500) {
-      showVolumeOverlay = !showVolumeOverlay;
-      longPressTriggered = true;
-      clickCount = 0; 
+      if (!isSeeking) {
+        showVolumeOverlay = !showVolumeOverlay;
+        longPressTriggered = true;
+        clickCount = 0; 
+      }
     }
   }
 
-  if (currentRawBtn == HIGH && lastRawBtnState == LOW) {
-    if (!longPressTriggered) {
-      clickCount++;
-      lastClickReleaseTime = millis();
-    }
-  }
-  lastRawBtnState = currentRawBtn;
-
-  //processes varios clicks
   if (clickCount > 0 && (millis() - lastClickReleaseTime > 300)) {
     if (clickCount == 1) {
-      if (showVolumeOverlay) {
+      if (isSeeking) {
+        uint32_t fileDuration = audio.getAudioFileDuration();
+        if (fileDuration > 0) {
+          seekTargetSec = constrain(seekTargetSec, (uint32_t)0, fileDuration - 1);
+        }
+        
+        audio.setAudioPlayPosition(seekTargetSec); 
+        
+        if (!isPlaying) {
+          audio.pauseResume();
+          isPlaying = true;
+        }
+
+        isSeeking = false;
+      } else if (showVolumeOverlay) {
         showVolumeOverlay = false;
       } else if (currentScreen == SCREEN_MENU) {
         playTrack(currentTrackIndex);
@@ -624,25 +678,29 @@ void loop() {
           playTrack(getNextTrackIndex());
         } else if (selectedPlayerBtn == BTN_SHUFFLE) {
           isShuffle = !isShuffle; 
+        } else if (selectedPlayerBtn == BTN_SEEK) {
+          isSeeking = true;
+          seekTargetSec = audio.getAudioCurrentTime();
         }
       } else if (currentScreen == SCREEN_HELP) {
         currentScreen = SCREEN_MENU;
       }
     } 
     else if (clickCount == 2) {
-      currentScreen = SCREEN_MENU;
+      isSeeking = false;
       showVolumeOverlay = false;
+      currentScreen = SCREEN_MENU;
     } 
     else if (clickCount >= 3) {
-      currentScreen = SCREEN_HELP;
+      isSeeking = false;
       showVolumeOverlay = false;
+      currentScreen = SCREEN_HELP;
     }
 
     clickCount = 0;
   }
 }
 
-//audio callback
 void audio_eof_mp3(const char *info) {
   playTrack(getNextTrackIndex());
 }
